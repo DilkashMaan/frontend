75766244702f20f80b4238f2f8a0e483db4346ac
Dilkash Maan <dilkashmaan7@gmail.com>
2025-05-29 17:02:45 +0530

    Update SocketProvider.jsx

--- src/context/SocketProvider.jsx
+++ src/context/SocketProvider.jsx
@@ -9,7 +9,7 @@ export const useSocket = () => {
 };
 
 export const SocketProvider = (props) => {
-  const socket = useMemo(() => io("https://webrtc-ox1p.onrender.com:8000"), []);
+  const socket = useMemo(() => io("https://webrtc-ox1p.onrender.com"), []);
 
   return (
     <SocketContext.Provider value={socket}>
